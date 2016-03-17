910fdf31ff7ce0e7513e37b36a7b1f0c30a90118
Stanislav Kalashnik <s.kalashnik@infomir.com>
2016-03-17 18:06:03 +0200

    rename global webpack flag DEBUG to DEVELOP

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function ( format ) {
     var args = Array.prototype.slice.call(arguments, 1),
         expr = /{(\d+)}/g;
 
-    if ( DEBUG ) {
+    if ( DEVELOP ) {
         if ( !format ) { throw new Error(__filename + ': format string is empty'); }
         if ( args.length === 0 )  { throw new Error(__filename + ': no arguments'); }
         if ( !expr.test(format) ) { throw new Error(__filename + ': format string does not have substitutions: ' + format); }
